b6c61e654510f424ed5340d155aed0bd6dcc3753
LaggeR <vo.binh224@gmail.com>
2020-11-26 21:23:16 +0700

    Add filter nhan vien

--- GUI_Clinic/View/UserControls/ucQuanLyNhanVien.xaml.cs
+++ GUI_Clinic/View/UserControls/ucQuanLyNhanVien.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BUS_Clinic.BUS;
+using DTO_Clinic.Person;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +23,55 @@ namespace GUI_Clinic.View.UserControls
     /// </summary>
     public partial class ucQuanLyNhanVien : UserControl
     {
+        public ObservableCollection<DTO_NhanVien> ListNV { get; set; }
         public ucQuanLyNhanVien()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            InitDataAsync();
+        }
+
+        private async Task InitDataAsync()
+        {
+            ListNV = await BUSManager.NhanVienBUS.GetListNVAsync();
+
+            lvNhanVien.ItemsSource = ListNV;
+
+            CollectionView viewNhanVien = (CollectionView)CollectionViewSource.GetDefaultView(lvNhanVien.ItemsSource);
+            viewNhanVien.Filter = NhanVienFilter;
+        }
+
+        private bool NhanVienFilter(Object item)
+        {
+            if (String.IsNullOrEmpty(tbxTimKiem.Text))
+            {
+                return true;
+            }
+            else
+            {
+                if (cbxLoaiTiemKiem.SelectedIndex == 0)
+                {
+                    return ((item as DTO_NhanVien).HoTen.IndexOf(tbxTimKiem.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                }
+                else if (cbxLoaiTiemKiem.SelectedIndex == 1)
+                {
+                    return ((item as DTO_NhanVien).MaNhanVien.IndexOf(tbxTimKiem.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                }
+                //else if (cbxLoaiTiemKiem.SelectedIndex == 2)
+                //{
+                //    return ((item as DTO_NhanVien).ChucVu.IndexOf(tbxTimKiem.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                //}
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         private void tbxTimKiem_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            CollectionViewSource.GetDefaultView(lvNhanVien.ItemsSource).Refresh();
         }
 
         private void lvNhanVien_SelectionChanged(object sender, SelectionChangedEventArgs e)
